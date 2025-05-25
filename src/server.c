74bbf5eee4c16c9c8f510d70832992a6fe23c8fa
Jean PINON <github@jean.pinon.name>
2025-05-25 20:55:10 +0200

    improve makefile

--- src/server.c
+++ src/server.c
@@ -10,18 +10,6 @@
 #include "server.h"
 
 
-// Port d'écoute de la socket
-#define PORT 8080
-// Adresse d'écoute (toutes les adresses)
-#define IP INADDR_ANY
-// Taille de la file d'attente
-#define BACKLOG 3
-// Nombre de connexions clients
-#define NB_CLIENTS 2
-// Taille du tampon de lecture des messages
-#define BUFFER_LEN 200
-// Commande pour arrêter le serveur
-#define EXIT_WORD "exit"
 
 
 
